b7e184a091136e66a034ebea32f1c95bc6fb6749
dubiefe <emiliedubief@free.fr>
2024-11-01 12:54:29 +0100

    Delete mail.php

--- mail.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
-    $from = $_POST['email'];
-    $retour = mail('emilie.dubief@etu.univ-grenoble-alpes.fr', $subject, $message, 'From: '.$from);
-    header('Location: ./apropos.html');
-    exit();
-?>
